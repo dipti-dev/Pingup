ceeb220d84e415b9a9e8c6a12bd4dc2e5dcd680c
dipti-dev <pald45198@gmail.com>
2026-01-30 20:46:10 +0530

    Fix Inngest route - use actual Inngest handler

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import inngestSimple from './inngest-simple.js';
+import { serve } from "inngest/express";
+import { inngest, functions } from "../server/inngest/index.js";
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Server is running'));
-app.use('/api/inngest', inngestSimple);
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 // Error handling middleware
 app.use((error, req, res, next) => {
